136847c962cee567586dd3ee76a5f3dbb58196c6
LavannyaManeesh <lavannyalavz@gmail.com>
2026-08-05 23:47:08 +0530

    Add files via upload
    Day 2 Assignment 05-08-2026

---
+++ assignment.js
@@ -0,0 +1,17 @@
+const browserVersion = 'Chrome'
+function getBrowserVersion(){
+
+    
+    if(browserVersion = 'Chrome'){
+        
+        var browserVersion = 'Version121'
+        let browserVersion2 = 'Versionlet'
+        
+    }
+    
+    console.log('Outside the Block', browserVersion);
+    console.log('Outside the Block', browserVersion2);
+}
+
+getBrowserVersion()
+//console.log('Outside the function', browserVersion);
