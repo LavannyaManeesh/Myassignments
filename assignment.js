63d2ea3804627815e340fc65f706a07adc10104f
LavannyaManeesh <lavannyalavz@gmail.com>
2026-08-06 11:32:28 +0530

    DAY 1 ASSIGNMENT

--- assignment.js
+++ assignment.js
@@ -1,17 +1,23 @@
-const browserVersion = 'Chrome'
-function getBrowserVersion(){
 
-    
-    if(browserVersion = 'Chrome'){
-        
-        var browserVersion = 'Version121'
-        let browserVersion2 = 'Versionlet'
-        
-    }
-    
-    console.log('Outside the Block', browserVersion);
-    console.log('Outside the Block', browserVersion2);
-}
+//STRING
+var firstName = 'Lavannya123'
+console.log(firstName ,"Typeof firstName:" + typeof firstName);
 
-getBrowserVersion()
-//console.log('Outside the function', browserVersion);
+var companyName = 'TCS'
+console.log(companyName);
+console.log("Typeof companyName:" + typeof companyName);
+
+//NUMBER
+var phoneNumber = 9400611324
+console.log(phoneNumber);
+console.log("Typeof phonenumber:" + typeof phoneNumber);
+
+//BOOLEAN
+var isAutomation = true
+console.log(isAutomation);
+console.log("Typeof isAutomation:" + typeof isAutomation);
+
+//UNDEFINED
+var hasPlaywright
+console.log(hasPlaywright);
+console.log("Typeof hasPlaywright:" + typeof hasPlaywright);
